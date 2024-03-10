f53582860e38c0c59dd4726e8da71068a7f86c5d
kelvinpraises <kelvinpraises@gmail.com>
2024-03-10 09:50:30 +0100

    chore: remove chakra

--- src/components/signIn.tsx
+++ src/components/signIn.tsx
@@ -1,32 +1,21 @@
-import Head from "next/head";
-import { Box, Text, VStack } from "@chakra-ui/react";
-import { type Doc, initJuno, setDoc, authSubscribe, User, signIn, InternetIdentityProvider } from "@junobuild/core-peer";
-
-import { BsFillTriangleFill } from "react-icons/bs"
-
+import { InternetIdentityProvider, signIn } from "@junobuild/core-peer";
 
 export default function LoginButton() {
+  const _signIn = async () => {
+    await signIn({
+      provider: new InternetIdentityProvider({
+        domain: "ic0.app",
+      }),
+    });
+  };
 
-    const _signIn = async () => {
-        await signIn({
-            provider: new InternetIdentityProvider({
-                domain: "ic0.app"
-            })
-        });
-    }
-
-    return (
-        <>
-            <Box mt={32}>
-                <VStack h="100%" alignItems={"center"} justifyContent={"center"}>
-                    <BsFillTriangleFill style={{ color: 'orange' }} />
-                    <Text as="button" color="blue" textDecoration={"underline"} onClick={_signIn}>Please sign in</Text>
-
-                </VStack>
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <div>
+          <p>Please sign in</p>
+        </div>
+      </div>
+    </>
+  );
 }
-
-
-// 2347268
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
-import Head from "next/head";
-import { Inter } from "next/font/google";
-import { Box, Text } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
-import { LogoCrown } from "@/utils/logoCrown";
-import { listUserDocs } from "@/utils/api";
-import { useGlobalContext } from "@/contexts/authContext";
 import ChatBubble from "@/components/chatBubble";
 import LoginButton from "@/components/signIn";
-
-
+import { useGlobalContext } from "@/contexts/authContext";
+import { listUserDocs } from "@/utils/api";
+import { LogoCrown } from "@/utils/logoCrown";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-
   const [loading, setLoading] = useState(false);
   const [responsePair, setResponsePair] = useState<any | undefined>(undefined);
 
@@ -33,8 +28,6 @@ export default function Home() {
     }
   }, [user]);
 
-
-
   return (
     <>
       <Head>
@@ -44,23 +37,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Box>
-
+        <div>
           {!responsePair && (
-            <Box w="100%" px={6} py={2}>‰
-                <LogoCrown />
-              <Text mt={4} textAlign={"center"} fontStyle={"italic"} fontSize={"sm"}>Your food companion for every occasion...</Text>
+            <div>
+              <LogoCrown />
+              <p>Your food companion for every occasion...</p>
               {!user && (
                 <>
                   <LoginButton />
                 </>
               )}
-            </Box>
+            </div>
           )}
 
           {responsePair && <ChatBubble history={responsePair} />}
-
-        </Box>
+        </div>
       </main>
     </>
   );
