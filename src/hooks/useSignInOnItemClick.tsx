845c951142cd4eaf6fdabd756395dfdcd5b3ce44
kelvinpraises <kelvinpraises@gmail.com>
2024-03-10 14:48:07 +0100

    feat: add sign in detector

---
+++ src/hooks/useSignInOnItemClick.tsx
@@ -0,0 +1,58 @@
+import { useGlobalContext } from "@/contexts/authContext";
+import {
+  InternetIdentityProvider,
+  User,
+  authSubscribe,
+  signIn,
+} from "@junobuild/core-peer";
+import { useEffect, useState } from "react";
+
+// Custom hook to handle sign-in logic for all clickable elements
+export const useSignInOnItemClick = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const cancelAuthListener = authSubscribe((user: User | null) => {
+      if (user) {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    });
+
+    return () => {
+      cancelAuthListener();
+    };
+  }, []);
+
+  const [showModal, setShowModal] = useState(false);
+  const _signIn = async () => {
+    await signIn({
+      provider: new InternetIdentityProvider({
+        domain: "ic0.app",
+      }),
+    });
+  };
+
+  const handleDocumentClick = (event: any) => {
+    // Check if user clicked on a clickable element
+    const isButtonClicked =
+      event.target.tagName === "BUTTON" || event.target.closest("button");
+
+    console.log(isButtonClicked);
+
+    if (isButtonClicked && loggedIn === false) {
+      event.stopPropagation();
+      _signIn();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleDocumentClick, true); // Event capturing
+    return () => {
+      document.removeEventListener("click", handleDocumentClick, true); // Cleanup
+    };
+  }, []);
+
+  return { showModal, setShowModal };
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import GlobalProvider from "@/contexts/authContext";
+import { useSignInOnItemClick } from "@/hooks/useSignInOnItemClick";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useSignInOnItemClick();
+
   return (
     <>
       <GlobalProvider>
