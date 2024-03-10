c66aafbcefbc5ddd1dd0b5cae3a033c74d5dba64
kelvinpraises <kelvinpraises@gmail.com>
2024-03-10 15:11:46 +0100

    feat: add utils index

---
+++ src/utils/index.ts
@@ -0,0 +1,22 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function isEmpty(input: any): boolean {
+  if (input === null || input === undefined) {
+    return true;
+  }
+
+  if (typeof input === "object") {
+    return Object.keys(input).length === 0;
+  }
+
+  if (Array.isArray(input)) {
+    return input.length === 0;
+  }
+
+  return false;
+}
