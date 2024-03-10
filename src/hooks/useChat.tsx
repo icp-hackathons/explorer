3d5e1eabe8d23be3016db1ab4bd7a6cf9779ed1e
kelvinpraises <kelvinpraises@gmail.com>
2024-03-10 19:09:14 +0100

    feat: prettify

--- src/hooks/useChat.tsx
+++ src/hooks/useChat.tsx
@@ -1,58 +1,67 @@
-import { useState, useEffect } from 'react';
 import { insertPromptResponse } from "@/utils/api";
+import { useEffect, useState } from "react";
 
 export const useChatHook = (initialHistory: any) => {
-    const [tempPairs, setTempPairs] = useState<any>([]);
-    const [value, setValue] = useState("");
-    const [response, setResponse] = useState(null);
-    const [responding, setResponding] = useState(false);
+  const [tempPairs, setTempPairs] = useState<any>([]);
+  const [value, setValue] = useState("");
+  const [response, setResponse] = useState(null);
+  const [responding, setResponding] = useState(false);
 
-    useEffect(() => {
-        console.log("Preloading history:", initialHistory);
-        if (initialHistory) {
-            setTempPairs([...initialHistory]);
-        }
-    }, [initialHistory]);
+  useEffect(() => {
+    console.log("Preloading history:", initialHistory);
+    if (initialHistory) {
+      setTempPairs([...initialHistory]);
+    }
+  }, [initialHistory]);
 
-    const askQuestion = async () => {
-        setTempPairs([...tempPairs, { human_message: value, ai_message: null }]);
+  const askQuestion = async () => {
+    setTempPairs([...tempPairs, { human_message: value, ai_message: null }]);
 
-        setTimeout(() => {
-            setResponding(true);
-        }, 1000);
+    setTimeout(() => {
+      setResponding(true);
+    }, 1000);
 
-        const response = await fetch('/api/chat', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ messages: [...tempPairs, value], prompt: value }),
-        });
-        setValue("");
-        const data = await response.json();
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ messages: [...tempPairs, value], prompt: value }),
+    });
+    setValue("");
+    const data = await response.json();
 
-        if (data && data.message) {
-            const msg = data.message.startsWith("AI: ") ? data.message.substring(4).trim() : data.message.substring(4).trim();
-            setResponse(msg);
-        }
-    };
+    if (data && data.message) {
+      const msg = data.message.startsWith("AI: ")
+        ? data.message.substring(4).trim()
+        : data.message.substring(4).trim();
+      setResponse(msg);
+    }
+  };
 
-    const updateJunoStore = async (id: string, question: string, response: string) => {
-        TODO:// //replace undefined with actual user's id
-        await insertPromptResponse("undefined", question, response);
-    };
-    
+  const updateJunoStore = async (
+    id: string,
+    question: string,
+    response: string
+  ) => {
+    // //replace undefined with actual user's id
+    TODO: await insertPromptResponse("undefined", question, response);
+  };
 
-    useEffect(() => {
-        if (response) {
-            const updatedPairs = [...tempPairs];
-            updatedPairs[tempPairs.length - 1].ai_message = response;
-            setTempPairs(updatedPairs);
-            setResponding(false);
-            setResponse(null);
-            updateJunoStore("", updatedPairs[tempPairs.length - 1].human_message, response);
-        }
-    }, [response, tempPairs]);
+  useEffect(() => {
+    if (response) {
+      const updatedPairs = [...tempPairs];
+      updatedPairs[tempPairs.length - 1].ai_message = response;
+      setTempPairs(updatedPairs);
+      setResponding(false);
+      setResponse(null);
+      updateJunoStore(
+        "",
+        updatedPairs[tempPairs.length - 1].human_message,
+        response
+      );
+    }
+  }, [response, tempPairs]);
 
-    return { tempPairs, value, responding, setValue, askQuestion };
+  return { tempPairs, value, responding, setValue, askQuestion };
 };
